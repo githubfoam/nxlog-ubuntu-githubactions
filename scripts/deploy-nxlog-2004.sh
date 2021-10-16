450840ce263673f0a10aae9b55a026b65be422c4
githubfoam <lequestionis@gmail.com>
2021-10-16 22:51:46 +0300

    fix file

---
+++ scripts/deploy-nxlog-2004.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -eox pipefail #safety for script
+
+echo "##############################################"
+echo "# Install NXlog CE                           #"
+echo "##############################################"
+
+apt-get update -qq
+
+
+# Download NXLOG
+#https://nxlog.co/products/nxlog-community-edition/download
+wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.11.2190_ubuntu_focal_amd64.deb
