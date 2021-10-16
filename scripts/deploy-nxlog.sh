13d99b33b2d13dc3d40bfec0a015f20edbcdc44d
githubfoam <lequestionis@gmail.com>
2021-10-16 22:20:50 +0300

    augmentation

--- scripts/deploy-nxlog.sh
+++ scripts/deploy-nxlog.sh
@@ -18,48 +18,48 @@ wget http://archive.ubuntu.com/ubuntu/pool/main/p/perl/perl-modules-5.26_5.26.1-
 apt install ./perl-modules-5.26_5.26.1-6_all.deb
 
 # Download and install Openssl library
-wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
-apt install ./libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
+# wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
+# apt install ./libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
 
-# Download and install GNU dbm library
-wget http://archive.ubuntu.com/ubuntu/pool/main/g/gdbm/libgdbm5_1.14.1-6_amd64.deb
-apt install ./libgdbm5_1.14.1-6_amd64.deb
+# # Download and install GNU dbm library
+# wget http://archive.ubuntu.com/ubuntu/pool/main/g/gdbm/libgdbm5_1.14.1-6_amd64.deb
+# apt install ./libgdbm5_1.14.1-6_amd64.deb
 
-# Download and install Perl libraries
-wget http://archive.ubuntu.com/ubuntu/pool/main/p/perl/libperl5.26_5.26.1-6_amd64.deb
-apt install ./libperl5.26_5.26.1-6_amd64.deb
+# # Download and install Perl libraries
+# wget http://archive.ubuntu.com/ubuntu/pool/main/p/perl/libperl5.26_5.26.1-6_amd64.deb
+# apt install ./libperl5.26_5.26.1-6_amd64.deb
 
-echo "##############################################"
-echo "# Configure UFW Firewall                     #"
-echo "##############################################"
+# echo "##############################################"
+# echo "# Configure UFW Firewall                     #"
+# echo "##############################################"
 
-#  Allow HTTP(s)
-ufw allow 80
-ufw allow 443
+# #  Allow HTTP(s)
+# ufw allow 80
+# ufw allow 443
 
-# Allow syslog TCP/UDP in
-ufw allow 514
+# # Allow syslog TCP/UDP in
+# ufw allow 514
 
-# Allow syslog SSL in
-ufw allow 1514
+# # Allow syslog SSL in
+# ufw allow 1514
 
-# Allow ElasticSearch (not for production)
-ufw allow 9200
+# # Allow ElasticSearch (not for production)
+# ufw allow 9200
 
-echo "##############################################"
-echo "# Setup NXLOG                                #"
-echo "##############################################"
+# echo "##############################################"
+# echo "# Setup NXLOG                                #"
+# echo "##############################################"
 
 # Download NXLOG
 #https://nxlog.co/products/nxlog-community-edition/download
-wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb
-dpkg -i nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb # method1
+# wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb
+# dpkg -i nxlog-ce_2.10.2150_ubuntu_bionic_amd64.deb # method1
 
 # wget http://nxlog.org/system/files/products/files/1/nxlog-ce_2.8.1248_ubuntu_1404_amd64.deb
 # dpkg -i nxlog-ce_2.8.1248_ubuntu_1404_amd64.deb
-apt-get install -f
+# apt-get install -f
 
-service nxlog status
+# service nxlog status
 
 # Remove default NXLOG configuration & replace
 # rm /etc/nxlog/nxlog.conf 
