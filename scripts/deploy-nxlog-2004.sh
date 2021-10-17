0ca87054c1a2785a3ea95536f7c743854f982e81
githubfoam <lequestionis@gmail.com>
2021-10-17 23:28:22 +0300

    liee

--- scripts/deploy-nxlog-2004.sh
+++ scripts/deploy-nxlog-2004.sh
@@ -23,7 +23,7 @@ sudo mv /etc/nxlog/nxlog.conf /etc/nxlog/nxlog.conf.orig
 
 sudo systemctl is-active nxlog >/dev/null 2>&1 && echo YES || echo NO
 
-sudo systemctl list-units --type=service #only active service units
+sudo systemctl list-units --type=service | grep nxlog #only active service units
 sudo systemctl list-dependencies nxlog.service #Displaying Dependencies
 
 sudo systemctl show nxlog.service #Checking Unit Properties
@@ -33,7 +33,7 @@ sudo systemctl show nxlog.service #Checking Unit Properties
 ls -lai /etc/rc*.d/
 
 #where "+" means service is in running state "-" is in stopped.
-service --status-all
+service --status-all | grep nxlog 
 
 # echo deb http://download.virtualbox.org/virtualbox/debian xenial contrib | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
 echo "deb http://archive.ubuntu.com/ubuntu/ trusty main universe restricted multiverse" | sudo tee -a /etc/apt/sources.list
@@ -43,7 +43,7 @@ sudo apt-get update
 sudo apt-get install sysv-rc-conf -y
 # show all services in all the runlevels
 sudo sysv-rc-conf --list
-sudo sysv-rc-conf --list nxlog 
+sudo sysv-rc-conf --list nxlog #0:off	1:off	2:on	3:on	4:on	5:on	6:off
 
 # choose which service should be active in boot time
 # sysv-rc-conf [--level <runlevels>] <service name> <on|off>
