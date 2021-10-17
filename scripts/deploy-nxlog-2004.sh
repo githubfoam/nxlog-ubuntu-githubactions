53117e69695a13d85624cfb39dc30f834c6c11fc
githubfoam <lequestionis@gmail.com>
2021-10-17 12:53:11 +0300

    frequence

--- scripts/deploy-nxlog-2004.sh
+++ scripts/deploy-nxlog-2004.sh
@@ -15,3 +15,22 @@ apt-get update -qq
 #https://nxlog.co/products/nxlog-community-edition/download
 wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.11.2190_ubuntu_focal_amd64.deb
 sudo apt install ./nxlog-ce_2.11.2190_ubuntu_focal_amd64.deb
+
+# The default location for the nxlog.conf file
+stat /etc/nxlog/nxlog.conf
+
+sudo mv /etc/nxlog/nxlog.conf /etc/nxlog/nxlog.conf.orig
+
+sudo systemctl is-active nxlog >/dev/null 2>&1 && echo YES || echo NO
+
+sudo systemctl list-units --type=service #only active service units
+sudo systemctl list-dependencies nxlog.service #Displaying Dependencies
+
+sudo systemctl show nxlog.service #Checking Unit Properties
+
+chkconfig --list #shows SysV services only and does not include native systemd services.
+chkconfig --list nxlog #verify that the run level configured correctly for the nxlog service
+chkconfig nxlog on #configure it to start Automatically while system start-up
+
+sudo systemctl status nxlog
+sudo systemctl status nxlog.service -l --no-pager
