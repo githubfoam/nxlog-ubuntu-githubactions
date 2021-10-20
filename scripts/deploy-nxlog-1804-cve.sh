e0a9b903f8eb6fe6b37e15c2609577d9f6e4a0c0
githubfoam <lequestionis@gmail.com>
2021-10-21 00:21:26 +0300

    circulation

--- scripts/deploy-nxlog-1804-cve.sh
+++ scripts/deploy-nxlog-1804-cve.sh
@@ -6,6 +6,20 @@ set -o xtrace
 # set -eox pipefail #safety for script
 
 echo "##############################################"
+echo "# Install NXlog CE                           #"
+echo "##############################################"
+
+apt-get update -qq
+
+
+# Download NXLOG
+#https://nxlog.co/products/nxlog-community-edition/download
+# wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.11.2190_ubuntu_focal_amd64.deb
+# sudo apt-get install ./nxlog-ce_2.11.2190_ubuntu_focal_amd64.deb
+wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.11.2190_ubuntu_bionic_amd64.deb
+sudo apt-get install ./nxlog-ce_2.11.2190_ubuntu_bionic_amd64.deb
+
+echo "##############################################"
 echo "# run cve test                               #"
 echo "##############################################"
 
