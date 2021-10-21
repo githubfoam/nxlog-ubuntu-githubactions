3bad0222308f375469ba2cbd52926ab884e50854
githubfoam <lequestionis@gmail.com>
2021-10-21 23:46:57 +0300

    init

--- scripts/deploy-nxlog-1804-cve.sh
+++ scripts/deploy-nxlog-1804-cve.sh
@@ -14,8 +14,6 @@ apt-get update -qq
 
 # Download NXLOG
 #https://nxlog.co/products/nxlog-community-edition/download
-# wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.11.2190_ubuntu_focal_amd64.deb
-# sudo apt-get install ./nxlog-ce_2.11.2190_ubuntu_focal_amd64.deb
 wget https://nxlog.co/system/files/products/files/348/nxlog-ce_2.11.2190_ubuntu_bionic_amd64.deb
 sudo apt-get install ./nxlog-ce_2.11.2190_ubuntu_bionic_amd64.deb
 
@@ -31,8 +29,6 @@ sudo systemctl status nxlog
 lsof -i :514
 
 git clone https://github.com/GuillaumePetit84/CVE-2020-35488.git
-ls -lai
 cd CVE-*
 ls -lai
-# sudo ./PoC.py -ip 192.168.1.55 -os 3
-python PoC.py -ip 192.168.1.55 -os 3
+
